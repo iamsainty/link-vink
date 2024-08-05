27af09b0a245f17dc1ab5c3e1da0f2a887a542cf
Priyanshu Chaurasiya <sainty@Priyanshus-Air.Dlink>
2024-08-05 10:52:35 +0530

    social media handles are now stored with user's profile

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,86 +1,129 @@
-const jwt = require('jsonwebtoken');
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const userdetails = require('../middleware/userdetails');
+const bcrypt = require("bcryptjs");
+const userdetails = require("../middleware/userdetails");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Register a user using POST
-router.post('/register', async (req, res) => {
-    try {
-        const salt = await bcrypt.genSalt(10); // Generate the salt
-        const safepassword = await bcrypt.hash(req.body.password, salt); // Encrypting the password with the salt
-        const user = await User.create({
-            name: req.body.name,
-            username: req.body.username,
-            password: safepassword,
-        });
-        const data = {
-            user: {
-                id: user.id,
-                username:  user.username,
-            }
-        }
-        const token = jwt.sign(data, JWT_SECRET)
-        res.status(201).json({ message: 'User created!', data: user, token: token });
-    } catch (err) {
-        res.status(400).json({ message: "Failed to create the user.", error: err });
-    }
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10); // Generate the salt
+    const safepassword = await bcrypt.hash(req.body.password, salt); // Encrypting the password with the salt
+    const user = await User.create({
+      name: req.body.name,
+      username: req.body.username,
+      password: safepassword,
+    });
+    const data = {
+      user: {
+        id: user._id,
+      },
+    };
+    const token = jwt.sign(data, JWT_SECRET);
+    res
+      .status(201)
+      .json({ message: "User created!", data: user, token: token });
+  } catch (err) {
+    res.status(400).json({ message: "Failed to create the user.", error: err });
+  }
 });
 
 // Login a user using POST
 router.post("/login", async (req, res) => {
-    try {
-        let user = await User.findOne({ username: req.body.username });
-        if (!user) return res.status(401).json({ message: "User does not exist" });
+  try {
+    let user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(401).json({ message: "User does not exist" });
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        if (!validPassword) return res.status(401).json({ message: "Invalid username or password" });
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(401).json({ message: "Invalid username or password" });
 
-        const data = {
-            user: {
-                id: user.id, // user.id is the id of the user
-                username: user.username
-            }
-        }
-        const token = jwt.sign(data, JWT_SECRET)
-        res.status(200).json({ token: token, data: user });
-    } catch (error) {
-        res.status(500).json({ message: "An error occurred during login" });
-    }
+    const data = {
+      user: {
+        id: user._id, // user._id is the id of the user
+      },
+    };
+    const token = jwt.sign(data, JWT_SECRET);
+    res.status(200).json({ token: token, data: user });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred during login" });
+  }
 });
 
-//route to fetch a single user from username
-router.get('/user/:username', async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.params.username });
-        if (!user) return res.status(404).json({ message: "User not found", exists: false }); // include 'exists' property
-        // update profile views
-        user.profileViews += 1;
-        res.status(200).json({ message: "User found", data: user, exists: true }); // include 'exists' property
-    } catch (err) {
-        res.status(500).json({ message: "An error occurred during fetching the user" });
-    }
+// Route to fetch a single user by ObjectId
+router.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user)
+      return res.status(404).json({ message: "User not found", exists: false }); // include 'exists' property
+    // update profile views
+    user.profileViews += 1;
+    await user.save();
+    res.status(200).json({ message: "User found", data: user, exists: true }); // include 'exists' property
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "An error occurred during fetching the user" });
+  }
 });
-
 
 // Get user details using Token with POST
-router.get('/userdetails', userdetails, async (req, res) => {
-    try {
-        const userId = req.user.id;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(401).json({ message: "User does not exist" });
-        }
-        res.status(200).json({ data: user });
-    } catch (error) {
-        console.error('Error fetching user details:', error.message);
-        res.status(500).send("Some Error occurred");
+router.get("/userdetails", userdetails, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({ message: "User does not exist" });
     }
+    res.status(200).json({ data: user });
+  } catch (error) {
+    console.error("Error fetching user details:", error.message);
+    res.status(500).send("Some Error occurred");
+  }
 });
 
+// Route to count the number of users registered
+router.get("/usercount", async (req, res) => {
+  try {
+    const usercount = await User.countDocuments();
+    res.status(200).json({ usercount });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred during counting the users" });
+  }
+});
 
+// Update social media accounts using PUT
+router.put("/updatesocial",  userdetails, async (req, res) => {
+    const { whatsapp, instagram, x, telegram, snapchat } = req.body.socialHandles;
+    const userId = req.user.id; // Assuming you have user ID available in req.user
+  
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      user.socialHandles.whatsapp = whatsapp;
+      user.socialHandles.instagram = instagram;
+      user.socialHandles.x = x;
+      user.socialHandles.telegram = telegram;
+      user.socialHandles.snapchat = snapchat;
+  
+      await user.save();
+      res.status(200).json({ message: "Social handles updated successfully", socialHandles: user.socialHandles });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+  
 
 module.exports = router;
--- server/routes/link.js
+++ server/routes/link.js
@@ -1,117 +1,106 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Link = require('../models/Link');
-const userdetails = require('../middleware/userdetails');
+const Link = require("../models/Link");
+const userdetails = require("../middleware/userdetails");
 
-// Fetching all links
-router.get('/links', async (req, res) => {
-    try {
-        const links = await Link.find();
-        res.status(200).json({ links });
-    } catch (error) {
-        console.error('Error fetching links:', error);
-        res.status(500).json({ message: 'Failed to fetch user links' });
-    }
+// Fetching all links along with link count and total click count
+router.get("/links", async (req, res) => {
+  try {
+    const links = await Link.find();
+    const linkcount = links.length;
+    const clickcount = links.reduce((acc, link) => acc + link.clickCount, 0);
+    res.status(200).json({ links, linkcount, clickcount });
+  } catch (error) {
+    console.error("Error fetching links:", error);
+    res.status(500).json({ message: "Failed to fetch links" });
+  }
 });
 
-//fetching all links of a single user
-router.get('/links/:username', async (req, res) => {
-    try {
-        const links = await Link.find({ username: req.params.username });
-        res.status(200).json({ links });
-    } catch (error) {
-        console.error('Error fetching links:', error);
-        res.status(500).json({ message: 'Failed to fetch user links' });
-    }
+// Fetching all links of a single user
+router.get("/userlinks/:id", async (req, res) => {
+  try {
+    const userId = req.params.id; // Get user ID from route parameters
+    const links = await Link.find({ user: userId }); // Use user ID to find links
+    const linkcount = links.length;
+    const clickcount = links.reduce((acc, link) => acc + link.clickCount, 0); // Calculate total click count
+    res.status(200).json({ links, linkcount, clickcount });
+  } catch (error) {
+    console.error("Error fetching links:", error);
+    res.status(500).json({ message: "Failed to fetch user links" });
+  }
 });
 
 // Fetching a single link by ID without updating click count
-router.get('/:id', async (req, res) => {
-    try {
-        const link = await Link.findById(req.params.id);
-        if (!link) return res.status(404).send("No such link exists.");
-        res.json({ link });
-    } catch (error) {
-        console.error('Error fetching link:', error);
-        res.status(500).send('Error fetching link');
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const link = await Link.findById(req.params.id);
+    if (!link) return res.status(404).send("No such link exists.");
+    res.json({ link });
+  } catch (error) {
+    console.error("Error fetching link:", error);
+    res.status(500).send("Error fetching link");
+  }
 });
 
 // Endpoint to handle link clicks and update click count
-router.post('/click/:id', async (req, res) => {
-    try {
-        const link = await Link.findById(req.params.id);
-        if (!link) return res.status(404).send("No such link exists.");
-        link.clickCount += 1;
-        await link.save();
-        res.json({ message: "Click count updated", clickCount: link.clickCount });
-    } catch (error) {
-        console.error('Error updating click count:', error);
-        res.status(500).send('Error updating click count');
-    }
+router.post("/click/:id", async (req, res) => {
+  try {
+    const link = await Link.findById(req.params.id);
+    if (!link) return res.status(404).send("No such link exists.");
+    link.clickCount += 1;
+    await link.save();
+    res.json({ message: "Click count updated", clickCount: link.clickCount });
+  } catch (error) {
+    console.error("Error updating click count:", error);
+    res.status(500).send("Error updating click count");
+  }
 });
 
 // Adding a new link
-router.post('/newlink', userdetails, async (req, res) => {
-    try {
-        const { title, url, social } = req.body;
-        const link = new Link({
-            username: req.user.username,
-            title,
-            url,
-            social,
-        });
-        const savedLink = await link.save();
-        res.json({ savedLink });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json("Some error occurred");
-    }
+router.post("/newlink", userdetails, async (req, res) => {
+  try {
+    const { title, url } = req.body;
+    const link = new Link({
+      user: req.user.id,
+      title,
+      url,
+    });
+    const savedLink = await link.save();
+    res.json({ savedLink });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Some error occurred");
+  }
 });
-
-
-//updating link order
-// Assuming you have an Express app setup
-
-router.put('/updateOrder', async (req, res) => {
-    const { links } = req.body;
-
-    try {
-        for (let i = 0; i < links.length; i++) {
-            await Link.findByIdAndUpdate(links[i]._id, { order: i });
-        }
-        res.json({ success: true });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Error updating link order', error });
-    }
-});
-
 
 // Updating a link
-router.put('/edit/:id', userdetails, async (req, res) => {
-    try {
-        const { title, url, category } = req.body;
-        const updatedFields = {
-            title,
-            url,
-            category,
-            lastUpdated: Date.now()
-        };
-        const updatedLink = await Link.findByIdAndUpdate(req.params.id, { $set: updatedFields }, { new: true });
-        res.json({ updatedLink });
-    } catch (error) {
-        res.status(500).send("Some error occurred");
-    }
+router.put("/edit/:id", userdetails, async (req, res) => {
+  try {
+    const { title, url } = req.body;
+    const updatedFields = {
+      title,
+      url,
+      lastUpdated: Date.now(),
+    };
+    const updatedLink = await Link.findByIdAndUpdate(
+      req.params.id,
+      { $set: updatedFields },
+      { new: true }
+    );
+    res.json({ updatedLink });
+  } catch (error) {
+    res.status(500).send("Some error occurred");
+  }
 });
 
 // Deleting a link
-router.delete('/delete/:id', userdetails, async (req, res) => {
-    try {
-        await Link.findByIdAndDelete(req.params.id);
-        res.json({ message: "Link has been deleted successfully" });
-    } catch (error) {
-        res.status(500).send("Some error occurred");
-    }
+router.delete("/delete/:id", userdetails, async (req, res) => {
+  try {
+    await Link.findByIdAndDelete(req.params.id);
+    res.json({ message: "Link has been deleted successfully" });
+  } catch (error) {
+    res.status(500).send("Some error occurred");
+  }
 });
 
 module.exports = router;
