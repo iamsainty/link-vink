67412c6ffc83bf5ccd9464a985df50dc6f2a9134
Priyanshu Chaurasiya <sainty@Priyanshus-MacBook-Air.local>
2024-08-08 17:31:29 +0530

    updated meta data on admin page

--- client/src/components/AdminPanel/AdminProfile/AdminProfile.js
+++ client/src/components/AdminPanel/AdminProfile/AdminProfile.js
@@ -8,6 +8,7 @@ import UserStats from "./UserStats";
 import UserSocial from "./UserSocial";
 import EditProfile from "./EditProfile";
 import AdminFooter from "./AdminFooter";
+import { Helmet } from "react-helmet-async";
 
 const Container = styled.div`
   margin: 0 auto;
@@ -26,6 +27,27 @@ function AdminProfile() {
 
   return (
     <>
+      <Helmet>
+        <title>{`Admin - Link Vink`}</title>
+        <meta name="description" content={"Admin Profile on Link Vink."} />
+        <meta
+          name="keywords"
+          content={`Admin, Link Vink, Profile, Social Links`}
+        />
+        <meta property="og:title" content={`Admin Profile - Link Vink`} />
+        <meta
+          property="og:description"
+          content={"Admin Profile on Link Vink."}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={window.location.href} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`Admin Profile - Link Vink`} />
+        <meta
+          name="twitter:description"
+          content={"Admin Profile on Link Vink."}
+        />
+      </Helmet>
       <AdminNavbar />
       <Container>
         <MainIntro />
